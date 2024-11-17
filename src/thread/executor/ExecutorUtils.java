d87c2cd75877a800e44c2a409d35175ad17e5066
Woody <yuncheol92@naver.com>
2024-11-17 11:50:29 +0900

    스레드 풀과 Executor 프레임워크2

--- src/thread/executor/ExecutorUtils.java
+++ src/thread/executor/ExecutorUtils.java
@@ -19,4 +19,17 @@ public abstract class ExecutorUtils {
             log(executorService);
         }
     }
+
+    public static void printState(ExecutorService executorService, String taskName) {
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            final var pool = poolExecutor.getPoolSize();
+            final var active = poolExecutor.getActiveCount();
+            final var queued = poolExecutor.getQueue().size();
+            final var completedTask = poolExecutor.getCompletedTaskCount();
+            log(taskName + " -> [pool = " + pool + ", active=" + active + ", queuedTasks=" + queued + ", completedTask="
+                + completedTask + "]");
+        } else {
+            log(executorService);
+        }
+    }
 }
